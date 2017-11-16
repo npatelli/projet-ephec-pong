3776432eff116ca799854c3ba115425aef128307
dad95 <33152867+dad95@users.noreply.github.com>
2017-11-16 17:14:37 +0100

    Balle avec la javadoc

--- Balle.java
+++ Balle.java
@@ -1,6 +1,13 @@
 import java.awt.Color;
 import java.awt.Graphics;
 
+/**
+ * 
+ * @author Adelin Gauthier
+ * @version 3.0
+ *
+ */
+
 public class Balle {
 	double xVel, yVel, x, y;
 	
@@ -11,10 +18,18 @@ public class Balle {
 		yVel = getRandomSpeed() * getRandomDirection();
 	}
 	
+	/**
+	 * 
+	 * @return un chiffre aléatoire qui gèrera de la vitesse de départ de la balle
+	 */
 	public double getRandomSpeed() {
 		return (Math.random() * 3 + 2 );
 	}
 	
+	/**
+	 * 
+	 * @return un chiffre aléatoire qui gèrera de la direction de départ de la balle
+	 */
 	public int getRandomDirection() {
 		int rand = (int)(Math.random() * 2);
 		if (rand == 1) {
@@ -25,11 +40,21 @@ public class Balle {
 		}
 	}
 	
+	/**
+	 * Dessine la balle (couleur, taille)
+	 * @param g La balle
+	 */
 	public void draw(Graphics g) {
 		g.setColor(Color.white);
 		g.fillOval((int)x-10, (int)y-10, 20, 20);
 	}
 	
+	/**
+	 * Vérifie si il y a collision entre la balle et les pads. 
+	 * Si oui, changement de direction.
+	 * @param p1 Pad 1
+	 * @param p2 Pad 2
+	 */
 	public void checkPaddleCollision(Paddle p1, Paddle p2) {
 		if (x <= 50) {
 			if (y >= p1.getY() && y <= p1.getY() + 80) {
@@ -42,6 +67,10 @@ public class Balle {
 			}
 	}
 	
+		/**
+		 * Mouvement de la balle
+		 * Rebondit sur les bords horizontaux
+		 */
 	public void move() {
 		x += xVel;
 		y += yVel;
@@ -54,10 +83,18 @@ public class Balle {
 		}
 	}
 	
+	/**
+	 * 
+	 * @return le x sous forme d'un entier
+	 */
 	public int getX() {
 		return (int)x;
 	}
 	
+	/**
+	 * 
+	 * @return le y sous forme d'un entier
+	 */
 	public int getY() {
 		return (int)y;
 	}
