21df39effac67c894666b671ed61c3fdf08170cf
dad95 <33152867+dad95@users.noreply.github.com>
2017-12-05 17:53:56 +0100

    Add files via upload

---
+++ PadIA.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ * @author Gauthier Adelin
+ * @version 2
+ * @date 05/12/2017
+ *
+ */
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class PadIA implements Pad {
+	double y, yVel;
+	boolean upAccel, downAccel;
+	int player, x;
+	Ball b1;
+	
+	final double GRAVITY = 0.95;
+	public PadIA(int player, Ball b) {
+		upAccel = false;
+		b1 = b;
+		downAccel = false;
+		y = 210;
+		yVel = 0;
+		
+		if(player == 1)
+			x = 20;
+		else
+			x = 660;
+		
+	}
+	
+	/**
+	* 
+	* @return Crée un pad aux couleurs et dimensions spécifiées
+	* @todo Paramètres de couleur et de taille
+	*
+	*/
+	public void draw(Graphics g) {
+		g.setColor(Color.white);
+		g.fillRect(x, (int)y, 20, 80);
+		
+	}
+	
+	/**
+	* 
+	* @return Déplace le pad vers le haut ou le bas
+	* @todo Paramètres de vitesse
+	*
+	*/
+	public void move() {
+		y = b1.getY() - 40;
+		
+		if(y<0)
+			y=0;
+		if(y>420)
+			y=420;
+	}
+
+	public int getY() {
+		return (int)y;
+	}
+}
